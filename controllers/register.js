ee541397e467301cc97413e08e6bb5718723daa7
Martin Mackov <martin.mackov@outlook.com>
2021-02-15 21:51:55 +0100

    Include actual error in Register fail message

--- controllers/register.js
+++ controllers/register.js
@@ -25,7 +25,7 @@ const handleRegister = (db, bcrypt) => (req, res) => {
             .then(trx.commit)
             .catch(trx.rollback);
         })
-        .catch(err => res.status(400).json('Unable to register'));
+        .catch(err => res.status(400).json(`Unable to register: ${err}`));
     });
 }
 
